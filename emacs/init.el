7166cfe13a9021c37953e91a594551468e579037
tiancaiamao <tiancaiamao@gmail.com>
2022-04-08 20:05:25 +0800

    add acme dir

--- emacs/init.el
+++ emacs/init.el
@@ -12,7 +12,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(vterm evil-collection evil-magit yaml-mode use-package s pyim-wbdict nim-mode monokai-theme markdown-mode magit go-projectile exec-path-from-shell evil-leader)))
+   '(pyim vterm evil-collection evil-magit yaml-mode use-package s pyim-wbdict nim-mode monokai-theme markdown-mode magit go-projectile exec-path-from-shell evil-leader)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
