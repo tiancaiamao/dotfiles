b45ea81f3ce1854df6a2d2c2e9034ffafede91ba
tiancaiamao <tiancaiamao@gmail.com>
2025-03-02 10:44:04 +0800

    update use-package source and mv custome.el

--- emacs/init.el
+++ emacs/init.el
@@ -2,22 +2,11 @@
 ;; Replace the source to China for some reasons everyone knows here.
 (require 'package)
 (setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
-			 ("gnu"   . "http://elpa.emacs-china.org/gnu/")
-			 ("melpa" . "http://elpa.emacs-china.org/melpa/")))
+			 ("nongnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/nongnu/")
+			 ("melpa"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
 (package-initialize)
 
 (org-babel-load-file "~/.emacs.d/config.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(multi-vterm pyim vterm evil-collection evil-magit yaml-mode use-package s pyim-wbdict nim-mode monokai-theme markdown-mode magit go-projectile exec-path-from-shell evil-leader)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(put 'dired-find-alternate-file 'disabled nil)
+
+(setq custom-file "~/.emacs.d/custom.el")
+(load-file custom-file)
